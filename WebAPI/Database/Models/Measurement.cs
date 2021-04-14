794e7199e2cd053968df3418efd83db5918bab03
Marin Bilba <marynbilba@gmail.com>
2021-04-14 13:49:53 +0200

    Added initial models

---
+++ WebAPI/Database/Models/Measurement.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Database.Models
+{
+    public class Measurement
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Time { get; set; }
+        public string Date { get; set; }
+        public decimal Value { get; set; }
+        public Sensor Sensor { get; set; }
+
+    }
+}
---
+++ WebAPI/Database/Models/Sensor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Database.Models
+{
+    public class Sensor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Unit { get; set; }
+        public Farm Farm { get; set; }
+        public SensorType SensorType { get; set; }
+
+
+    }
+}
