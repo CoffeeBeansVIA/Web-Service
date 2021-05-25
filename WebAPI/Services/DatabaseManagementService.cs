a8af7b29dd127add98eed4c40efff46e7f06cf30
Eugeniu Racila <eugeniu.racila@gmail.com>
2021-05-25 20:35:49 +0200

    Add Docker support

---
+++ WebAPI/Services/DatabaseManagementService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPI.Database;
+
+namespace WebAPI.Services
+{
+    public class DatabaseManagementService
+    {
+        public static void MigrationInitialization(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<DataContext>()?.Database.Migrate();
+            }
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -47,6 +47,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            DatabaseManagementService.MigrationInitialization(app);
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
