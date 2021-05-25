d46968c5bf969afac2afa01dee86a7b8f6807f1a
Eugeniu Racila <eugeniu.racila@gmail.com>
2021-05-25 21:34:52 +0200

    Fix migration duplication

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -47,8 +47,6 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            DatabaseManagementService.MigrationInitialization(app);
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
